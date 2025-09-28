6862bc140da332193f645deca937519ce2cfc756
umemura <ume.daihuku0116.0707@gmail.com>
2025-09-28 15:58:25 +0900

    vuetify 追加

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -3,8 +3,19 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+// アイコンのCSSを追加
+import '@mdi/font/css/materialdesignicons.css'
 
 export const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 })
