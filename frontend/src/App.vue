c9c95d99b31edeb4e2a5174463cb348eec5cb866
umemura <ume.daihuku0116.0707@gmail.com>
2025-10-18 16:08:17 +0900

    フッター共通化

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <v-app>
     <AppHeader />
+      <v-snackbar
+        v-model="store.show"
+        :color="store.color"
+        timeout="3000"
+        location="top"
+      >
+        {{ store.message }}
+      </v-snackbar>
     <router-view />
+    <AppFooter />
   </v-app>
 </template>
 
 <script>
 import { defineAsyncComponent } from 'vue'
+import { useNotificationStore } from '@/stores/notification'
+
 export default {
   name: 'App',
   components: {
-    AppHeader: defineAsyncComponent(() => import('./components/AppHeader.vue'))
+    AppHeader: defineAsyncComponent(() => import('./components/AppHeader.vue')),
+    AppFooter: defineAsyncComponent(() => import('./components/AppFooter.vue'))
+  },
+  data() {
+    return {
+      store: useNotificationStore()
+    }
   }
 }
 </script>
---
+++ frontend/src/components/AppFooter.vue
@@ -0,0 +1,41 @@
+<template>
+<!-- フッター (PCのみ) -->
+  <v-footer
+    app
+    class="d-none d-md-flex justify-space-between px-6"
+    color="grey-lighten-4"
+  >
+    <p class="text-caption">利用規約 プライバシーポリシー</p>
+    <div class="d-flex gap-4">
+      <v-icon>mdi-twitter</v-icon>
+      <v-icon>mdi-facebook</v-icon>
+      <v-icon>mdi-instagram</v-icon>
+    </div>
+  </v-footer>
+
+  <!-- モバイル用ボトムナビ -->
+  <v-bottom-navigation
+    app
+    grow
+    class="d-md-none"
+    color="blue-darken-3"
+    active-color="white"
+  >
+    <v-btn>
+      <v-icon>mdi-home</v-icon>
+      ホーム
+    </v-btn>
+    <v-btn>
+      <v-icon>mdi-trophy</v-icon>
+      ランキング
+    </v-btn>
+    <v-btn>
+      <v-icon>mdi-plus</v-icon>
+      投稿
+    </v-btn>
+    <v-btn>
+      <v-icon>mdi-bell</v-icon>
+      お知らせ
+    </v-btn>
+  </v-bottom-navigation>
+</template>
